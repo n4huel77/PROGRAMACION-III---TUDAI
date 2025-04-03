eb40f719246c3afe9ad301d21e24348a4bfce74e
Nahuel Gulias <guliasnahuel07@gmail.com>
2025-04-03 20:17:07 -0300

    Empieza tp2

--- src/TP2/Ejercicio1.java
+++ src/TP2/Ejercicio1.java
@@ -19,4 +19,36 @@ public class Ejercicio1 {
         }
         return ordenado;
     }
+
+
+    public ArrayList<Integer> getBinario(int number){
+        ArrayList<Integer> resultado = new ArrayList<>();
+
+        if(number == 0 || number == 1){
+            resultado.add(number);
+            return resultado;
+        }
+
+        int division = number/2;
+        int resto = number - (division * 2);
+
+        number = division;
+        ArrayList<Integer> list = getBinario(number);
+        list.add(resto);
+        return list;
+    }
+
+                            //10            1           0
+    public static String fib(int contador, int actual, int prev) {
+        if (contador > 0) {// Primera iteracion con 10
+            return prev + " " + fib(contador - 1, actual + prev, actual);
+            // retorna 0 + {(0+1)} 1  + 1  en la primera vuelta
+        } else {
+            return "";
+        }
+    }
+    //Se llama en main asi
+    public static void main(String[] args) {
+        System.out.println(fib(10, 1, 0));
+    }
 }

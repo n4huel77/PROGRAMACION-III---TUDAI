dbbe41b2ca5cfdb3c1f0488738994f034d2d640f
Nahuel Gulias <guliasnahuel07@gmail.com>
2025-05-15 12:31:16 -0300

    Se agrega constructor en -DFSCaminoMasLargo-

--- src/Grafos/DFSCaminoMasLargo.java
+++ src/Grafos/DFSCaminoMasLargo.java
@@ -2,9 +2,26 @@ package Grafos;
 
 import java.util.Iterator;
 import java.util.LinkedList;
-
+/*
+Ejercicio 4
+    Escribir un algoritmo que, dado un grafo dirigido y dos vértices i, j de este grafo, devuelva el
+    camino simple (sin ciclos) de mayor longitud del vértice i al vértice j. Puede suponerse que el
+    grafo de entrada es acíclico.
+*/
 public class DFSCaminoMasLargo<T> {
     private LinkedList<Integer> caminoMasLargo;
+
+    public DFSCaminoMasLargo() {
+        this.caminoMasLargo = new LinkedList<>();
+    }
+
+    public LinkedList<Integer> obtenerCaminoMasLargo(GrafoDirigido<T> grafo, int vertice1, int vertice2){
+        caminoMasLargo.clear();
+        LinkedList<Integer> recorrido = new LinkedList<>();
+        recorrido.add(vertice1);
+        obtenerCaminoMasLargo(grafo,vertice1,vertice2,recorrido);
+        return caminoMasLargo;
+    }
 
     private void obtenerCaminoMasLargo(GrafoDirigido<T> grafo, int vertice1, int vertice2, LinkedList<Integer> recorridoAct){
         if(vertice1==vertice2) {
@@ -24,11 +41,4 @@ public class DFSCaminoMasLargo<T> {
 
     }
 
-    public LinkedList<Integer> obtenerCaminoMasLargo(GrafoDirigido<T> grafo, int vertice1, int vertice2){
-        caminoMasLargo.clear();
-        LinkedList<Integer> recorrido = new LinkedList<>();
-        recorrido.add(vertice1);
-        obtenerCaminoMasLargo(grafo,vertice1,vertice2,recorrido);
-        return caminoMasLargo;
-    }
 }
